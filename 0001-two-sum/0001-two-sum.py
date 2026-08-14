813f26af1fb50421f44fd55f72103bac6d1c68cb
Anil Sevakula <anilsevakula143@gmail.com>
2026-08-14 16:02:01 +0530

    Time: 0 ms (100%), Space: 20.5 MB (17.52%) - LeetHub - Time: O(n), Space: O(n)

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,5 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # Brute Force
+        # for i in range(len(nums) - 1):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # return []
+
+        #Optimised
         seen = {}
         for i in range(len(nums)):
             comp = target - nums[i]
