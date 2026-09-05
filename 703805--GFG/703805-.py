e7af01c220d4f84e83536121d990d4baf078e8bf
Anil Sevakula <anilsevakula143@gmail.com>
2026-09-05 13:46:29 +0530

    Added solution - Time: O(1), Space: O(1)

--- 703805--GFG/703805-.py
+++ 703805--GFG/703805-.py
@@ -1,9 +1,23 @@
 class Solution:
     def nextPowerOfTwo(self, n):
-        power = 1
-        while power < n:
-            power *= 2
-        return power
+
+        if n <= 1:
+            return 1
+
+        # Decrement n by 1.
+        n -= 1
+
+        # Propagate the most significant set bit to all lower positions.
+        n |= (n >> 1)
+        n |= (n >> 2)
+        n |= (n >> 4)
+        n |= (n >> 8)
+        n |= (n >> 16)
+
+        # Return the next power of 2.
+        return n + 1
+
+
         
 
 # Synced seamlessly with LeetHub Pro
