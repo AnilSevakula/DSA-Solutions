e127cee1d391a8f32564a1b682801d88337ae933
Anil Sevakula <anilsevakula143@gmail.com>
2026-09-05 12:43:33 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 701288-anagram-GFG/701288-anagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def areAnagrams(self, s, t):
+       d = {}
+       for i in range(65, 97+26):
+           d[chr(i)] = 0
+
+       for ch in s:
+           d[ch] = d.get(ch, 0) + 1
+
+       for ch in t:
+           d[ch] = d.get(ch, 0) - 1
+
+       for ch in d:
+           if d[ch] != 0:
+               return False
+       else:
+           return True
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
